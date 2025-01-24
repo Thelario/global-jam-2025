2f93946a5d1c1dba13b7bd82acebcd5e57934089
Pablo Lario Gómez <pablo.lario02@gmail.com>
2025-01-24 10:35:07 +0100

    Creating generic minigame duration control manager

--- GlobalGameJam2025/Assets/Developers/Pablo/Scripts/Shooting Bubbles/MinigameDurationControlManager.cs
+++ GlobalGameJam2025/Assets/Developers/Pablo/Scripts/Shooting Bubbles/MinigameDurationControlManager.cs
@@ -1,18 +1,28 @@
 using TMPro;
 using UnityEngine;
 
-public class ShootingBubblesManager : Singleton<ShootingBubblesManager>
+public class MinigameDurationControlManager : Singleton<MinigameDurationControlManager>
 {
+    public delegate void MinigameEnd();
+    public MinigameEnd OnMinigameEnd;
+    
     [Header("Minigame")]
     [SerializeField] private float minigameDuration = 1f;
     
     [Header("UI")]
     [SerializeField] private TMP_Text minigameDurationText;
 
+    private bool _minigameEnded;
     private float _minigameDurationCounter;
 
     private void Start()
     {
+        Configure();
+    }
+
+    public void Configure()
+    {
+        _minigameEnded = false;
         _minigameDurationCounter = minigameDuration * 60f;
 
         CalculateTimeText();
@@ -24,5 +34,20 @@ public class ShootingBubblesManager : Singleton<ShootingBubblesManager>
         int seconds = Mathf.FloorToInt(_minigameDurationCounter % 60f);
 
         minigameDurationText.text = $"{minutes:00}:{seconds:00}";
+    }
+
+    private void Update()
+    {
+        if (_minigameEnded)
+            return;
+        
+        _minigameDurationCounter -= Time.deltaTime;
+        CalculateTimeText();
+
+        if (!(_minigameDurationCounter <= 0f))
+            return;
+        
+        _minigameEnded = true;
+        OnMinigameEnd?.Invoke();
     }
 }
