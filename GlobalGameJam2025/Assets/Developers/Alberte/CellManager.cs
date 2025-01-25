2c87ac8f8198e491b0e7760214db20b6f15f618b
Vavaro97 <guaredrive@gmail.com>
2025-01-25 18:20:41 +0100

    CellsMinigame

---
+++ GlobalGameJam2025/Assets/Developers/Alberte/CellManager.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using UnityEngine;
+
+public class CellManager : MonoBehaviour
+{
+    public GameObject[] cells;
+    public bool randomizing = false;
+    public int level = 10;
+    
+    public Material[] materials;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cells = GameObject.FindGameObjectsWithTag("Cell");
+        StartCoroutine(Randomize());
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Randomize()
+    {
+
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            level--;
+            if (level == 1)
+            {
+                level = 1;
+            }
+            for (int x = 0; x < 10; x++)
+            {
+
+                int random = Random.Range(0, cells.Length);
+                while (cells[random].GetComponent<Cell>().trap)
+                {
+                    random = Random.Range(0, cells.Length);
+                }
+                cells[random].GetComponent<Cell>().trap = true;
+            }
+            for (int i = 0; i < cells.Length; i++)
+            {
+                if (cells[i].GetComponent<Cell>().trap == true)
+                {
+                    cells[i].GetComponent<Renderer>().material = materials[0];
+
+                }
+                else
+                {
+
+                    cells[i].GetComponent<Renderer>().material = materials[1];
+
+                }
+
+            }
+
+            yield return new WaitForSeconds(4f);
+            for (int i = 0; i < cells.Length; i++)
+            {
+                if (!cells[i].GetComponent<Cell>().trap)
+                {
+                    cells[i].SetActive(false);
+
+                }
+
+
+            }
+            yield return new WaitForSeconds(3);
+            for (int i = 0; i < cells.Length; i++)
+            {
+
+                cells[i].SetActive(true);
+                cells[i].GetComponent<Renderer>().material = materials[2];
+
+            }
+        }
+        
+
+
+
+
+
+
+
+        //while (randomizing)
+        //{
+
+        //    for (int i = 0; i < cells.Length; i++)
+        //    {
+        //        if (cells[i].GetComponent<Cell>().trap == true)
+        //        {
+        //            cells[i].GetComponent<Renderer>().material = materials[0];
+        //        }
+        //        else
+        //        {
+        //            cells[i].GetComponent<Renderer>().material = materials[1];
+        //        }
+
+        //    }
+        //    yield return new WaitForSeconds(0.3f);
+        //    randomizing = false;
+        //}
+        //Debug.Log("SALEEE");
+
+
+        //yield return new WaitForSeconds(0.3f);
+        //randomizing = false;
+        //for (int i = 0; i < cells.Length; i++)
+        //{
+        //    if (cells[i].GetComponent<Cell>().trap == true)
+        //    {
+        //        cells[i].GetComponent<Renderer>().material = materials[0];
+        //    }
+        //    else
+        //    {
+        //        cells[i].GetComponent<Renderer>().material = materials[1];
+        //    }
+
+        //}
+        //yield return new WaitForSeconds(0.3f);
+        //randomizing = false;
+
+
+        //for( int z=0; z<level; z++)
+        //{
+        //    int random = Random.Range(0, cells.Length);
+        //    while (cells[random].GetComponent<Cell>().trap)
+        //    {
+        //        random = Random.Range(0, cells.Length);
+        //    }
+        //    cells[random].GetComponent<Cell>().trap = true;
+        //    cells[random].GetComponent<Renderer>().material = materials[1];
+        //}
+        //for (int i = 0; i < cells.Length; i++)
+        //{
+        //    if (cells[i].GetComponent<Cell>().trap)
+        //    {
+        //        cells[i].GetComponent<Renderer>().material = materials[0];
+        //    }
+
+
+
+        //}
+        //yield return new WaitForSeconds(3f);
+        //for (int i = 0; i < cells.Length; i++)
+        //{
+        //    if (!cells[i].GetComponent<Cell>().trap)
+        //    {
+        //        cells[i].SetActive(false);
+        //    }
+
+
+
+        //}
+
+
+    }
+}
