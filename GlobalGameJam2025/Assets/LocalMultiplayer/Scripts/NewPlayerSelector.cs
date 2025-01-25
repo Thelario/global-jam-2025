10feb99779773ceeeed93e35a1c26a84e9d5fa35
IteyGames <93440670+IteyGames@users.noreply.github.com>
2025-01-25 19:09:30 +0100

    Base de Player Selector visuals

--- GlobalGameJam2025/Assets/LocalMultiplayer/Scripts/NewPlayerSelector.cs
+++ GlobalGameJam2025/Assets/LocalMultiplayer/Scripts/NewPlayerSelector.cs
@@ -2,28 +2,42 @@ using DG.Tweening;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewPlayerSelector : MonoBehaviour
 {
-    private GameObject playerPrefab;
     [Header("Players")]
-    [SerializeField] private List<Vector3> spawnPositions;
+    [SerializeField] private List<GameObject> playerVisuals;
+    [SerializeField] private Button gobackButton, continueButton; 
+
     private void OnEnable()
     {
         GameManager.Instance.OnPlayerAdded += UpdateUI;
         GameManager.Instance.OnPlayerRemoved += UpdateUI;
+        if (continueButton) gobackButton.onClick.AddListener(()=> SceneNav.GoTo(SceneType.MainMenuScene));
+        if (continueButton) continueButton.onClick.AddListener(TryChangeScene);
+    }
+    private void TryChangeScene()
+    {
+        if (GameManager.Instance.NumberOfPlayers() < 1) return;
+        SceneNav.GoTo(SceneType.GameSettings);
     }
     private void OnDisable()
     {
         GameManager.Instance.OnPlayerAdded -= UpdateUI;
         GameManager.Instance.OnPlayerRemoved -= UpdateUI;
     }
-    private void Start()
+    private void Awake()
     {
-        playerPrefab = AssetLocator.PlayerPrefab();
+        foreach (var player in playerVisuals) { player.SetActive(false); }
     }
     private void UpdateUI(PlayerData newPlayer = null)
     {
-        
+        foreach (var player in playerVisuals) { player.SetActive(false); }
+        int index = GameManager.Instance.NumberOfPlayers();
+        for (int i = 0; i < index && i < playerVisuals.Count; i++)
+        {
+            playerVisuals[i].SetActive(true);
+        }
     }
 }
