1695e082bb4d8ac600cbd2ad6fa599be7fa58eab
Pablo Lario Gómez <pablo.lario02@gmail.com>
2025-01-25 11:44:24 +0100

    Pinball popup

--- GlobalGameJam2025/Assets/Game/_Scripts/PinballCylinder.cs
+++ GlobalGameJam2025/Assets/Game/_Scripts/PinballCylinder.cs
@@ -1,9 +1,26 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class PinballCylinder : MonoBehaviour
 {
     [SerializeField] private float pinballForce;
+    [SerializeField] private float popupTime;
+    [SerializeField] private float scaleMultiplier;
+    [SerializeField] private Ease ease;
+    [SerializeField] private Transform target;
     
+    private Vector3 _originalScale;
+
+    private void Start()
+    {
+        _originalScale = target.localScale;
+    }
+
+    private void OnEnable()
+    {
+        _originalScale = target.localScale;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.TryGetComponent<PlayerController>(out var playerController) != true)
@@ -12,5 +29,22 @@ public class PinballCylinder : MonoBehaviour
         Vector3 hitDirection = other.transform.position - other.contacts[0].point;
 
         playerController.SetLinearVelocity(hitDirection * pinballForce);
+        PopUp();
+    }
+
+    private void PopUp()
+    {
+        _originalScale = target.localScale;
+        Vector3 newScale = new Vector3(_originalScale.x * scaleMultiplier, _originalScale.y,
+            _originalScale.z * scaleMultiplier);
+        
+        target.DOKill();
+        target.DOScale(newScale, popupTime).SetEase(ease).OnComplete(Reset);
+    }
+
+    private void Reset()
+    {
+        target.DOKill();
+        target.DOScale(_originalScale, popupTime / 2f).onComplete();
     }
 }
