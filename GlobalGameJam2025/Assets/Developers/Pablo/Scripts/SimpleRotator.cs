f1d4a3d8ebafd4da8e937efd0bdaaf5cac2428c1
Pablo Lario Gómez <pablo.lario02@gmail.com>
2025-01-25 02:13:57 +0100

    Fiuuuuuuuuuuuuuuuuuuuuuuummmm

--- GlobalGameJam2025/Assets/Developers/Pablo/Scripts/SimpleRotator.cs
+++ GlobalGameJam2025/Assets/Developers/Pablo/Scripts/SimpleRotator.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 public class SimpleRotator : MonoBehaviour
 {
     [SerializeField] float rotationSpeed;
-    float fiuuuum = 0f;
+    
+    private float _fiuuuum = 0f;
+    
     private void Update()
     {
-        //fiuuuum = Time.deltaTime;
-        
-        //transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed * fiuuuum);
-        transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+        _fiuuuum = Time.deltaTime;
+        transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed * _fiuuuum);
     }
 }
