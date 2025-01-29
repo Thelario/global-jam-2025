4f9d6f0c3ec2c6776124824b9f18b9cb212891ab
IteyGames <93440670+IteyGames@users.noreply.github.com>
2025-01-29 12:27:02 +0100

    Player Select UI Change

--- GlobalGameJam2025/Assets/LocalMultiplayer/Scripts/PlayerSelector.cs
+++ GlobalGameJam2025/Assets/LocalMultiplayer/Scripts/PlayerSelector.cs
@@ -1,5 +1,4 @@
 using DG.Tweening;
-using DG.Tweening.Core.Easing;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -8,13 +7,14 @@ using UnityEngine.UI;
 public class PlayerSelector : MonoBehaviour
 {
     [Header("Player Panel Window")]
-    [SerializeField] private TextMeshProUGUI numberPlayers;
+    [SerializeField] private TextMeshProUGUI playerCountText;
     [SerializeField] private List<PlayerProfileUI> playerList;
-    
+
     [Space(10)]
     [Header("UI Nav")]
-    [SerializeField] private Button gobackButton, continueButton; 
+    [SerializeField] private Button gobackButton, continueButton;
     
+
     //References
     GameManager gameManager;
 
@@ -27,6 +27,32 @@ public class PlayerSelector : MonoBehaviour
         if (gobackButton) gobackButton.onClick.AddListener(()=> SceneNav.GoTo(SceneType.MainMenuScene));
         if (continueButton) continueButton.onClick.AddListener(TryChangeScene);
     }
+    private void Start()
+    {
+        StartPlayerUIFX();
+        UpdatePlayerUI();
+    }
+
+    private void StartPlayerUIFX()
+    {
+        for (int i = 0; i < playerList.Count; i++)
+        {
+            playerList[i].transform.DOScale(Vector3.one * 1.025f, 0.3f).SetDelay(i * Random.Range(0.2f, 0.7f))
+                .SetLoops(-1, LoopType.Yoyo);
+        }
+    }
+    private void UpdatePlayerUI()
+    {
+        foreach (var cg in playerList) cg.SetProfile(0.2f);
+
+        List<PlayerData> allPlayers = GameManager.Instance.GetPlayerList();
+        for (int i = 0; i < allPlayers.Count; i++)
+        {
+            playerList[i].SetProfile(1.0f, allPlayers[i]);
+        }
+        playerCountText.text = $"Players {allPlayers.Count}/{GameSettings.MAX_PLAYERS}";
+    }
+
     private void TryChangeScene()
     {
         if (GameManager.Instance.PlayerCount < 1) return;
